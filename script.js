1e2c3a828bccd2fc6f09ab10a2dd617ff88b039d
yuriykarpoff <yuriykarpoff@gmail.com>
2023-08-16 22:17:07 +0500

    Готовое домашнее задание

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const productName = 'Samsung Galaxy s23';
+const productCount = 4;
+const productCategory = 'Мобильные телефоны';
+const productPrice = 65000;
+const productTotalPrice = productCount * productPrice;
+
+console.log(productName);
+console.log(productTotalPrice);
