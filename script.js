88d8681b965038358243974ae6c5358709c5e273
yuriykarpoff <yuriykarpoff@gmail.com>
2023-10-09 23:13:13 +0500

    start game

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(() => {
+    const FIGURES_ENG = ['rock', 'sissors', 'paper'];
+    const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+})();
+
+const game = (language) => {
+    const result = {
+        player: 0,
+        computer: 0,
+    };
+
+    return function start() {
+
+    };
+
+    window.RPS = game;
+};
--- task02.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const arrayStart = [];
-
-const generateArray = (array) => 
-  (array.reduce((acc, item) => acc + item, 0) >= 50 ? array : generateArray([...array, Math.round(Math.random() * 10)]));
-
-console.log(generateArray(arrayStart));
